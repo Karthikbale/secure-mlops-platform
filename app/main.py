77c6e4fbdaa194641186b86dffbe84b9b979567a
Karthik <karthikbale2002@gmail.com>
2026-08-11 16:33:49 +0530

    updated requirement.txt

--- app/main.py
+++ app/main.py
@@ -1,17 +1,24 @@
 from fastapi import FastAPI
 from app.api.routes import router
+from prometheus_fastapi_instrumentator import Instrumentator
+
 
 app = FastAPI(
     title="Secure MLOps Platform",
     version="1.0.0"
 )
 
+Instrumentator().instrument(app).expose(app)
+
+
 @app.get("/")
 def root():
     return {"message": "Welcome to Secure MLOps Platform"}
 
+
 @app.get("/health")
 def health():
     return {"status": "healthy"}
+
 
 app.include_router(router)
